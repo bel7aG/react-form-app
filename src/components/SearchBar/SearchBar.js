68d8cb6a31de23650564ac19072d36a1b2069aff
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-23 22:19:12 +0100

    form

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+
+  handleInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
       <div className="search-bar">
         <form>
           <label htmlFor="search-bar-input" className="search-name flex-center">Search</label>
-          <input type="text" className="search-bar-input flex-center" placeholder="search" />
+          <input
+            type="text"
+            className="search-bar-input flex-center"
+            placeholder="search"
+            onChange={this.handleInputChange}
+          />
         </form>
       </div>
     );
