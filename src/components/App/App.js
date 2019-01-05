5cf4567b768b4ac93ae67cb1bf16ffb00054bbc4
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-05 17:23:41 +0100

    using for the first time the async await syntax :D

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,22 +4,18 @@ import { SearchBar } from '../'
 
 export default class App extends Component {
   componentDidMount = () => {
-
   }
   
-  handleFormSubmit = (term) => {
-    axios.get('https://api.unsplash.com/search/photos', {
+  handleFormSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         query: term
       },
       headers: {
         Authorization: 'Client-ID 3f9a2cacd0cb48e872e4d9e5bfa722c5a0e109bf79d968d308a258ef25061462'
       }
-    }).then((response) => {
-      console.log(response.data.results)
-    }).catch((error) => {
-      console.log(error)
     })
+    console.log(response.data.results)
   }
 
   render() {
