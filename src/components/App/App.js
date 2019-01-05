470cab1e15874df9334f397e28d605900a09a68d
bel7aG <belhassen.gharsallah@isetrades.com>
2019-01-05 17:21:24 +0100

    first method get data using promises

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 import { SearchBar } from '../'
 
 export default class App extends Component {
+  componentDidMount = () => {
+
+  }
   
   handleFormSubmit = (term) => {
     axios.get('https://api.unsplash.com/search/photos', {
@@ -12,6 +15,10 @@ export default class App extends Component {
       headers: {
         Authorization: 'Client-ID 3f9a2cacd0cb48e872e4d9e5bfa722c5a0e109bf79d968d308a258ef25061462'
       }
+    }).then((response) => {
+      console.log(response.data.results)
+    }).catch((error) => {
+      console.log(error)
     })
   }
 
